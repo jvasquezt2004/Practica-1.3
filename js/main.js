4cd07e2edaa47db0ea7f04e15c0af1f1935dd653
nodexs0 <nodexs@outlook.com>
2024-03-20 17:42:42 -0600

    Creacion de la clase ProgramWindow, incluido el metodo resize

--- js/main.js
+++ js/main.js
@@ -22,20 +22,58 @@ class Position {
     }
 }
 
-const size = new Size(1080, 764);
-console.log(size.width);
-console.log(size.height);
+// const size = new Size(1080, 764);
+// console.log(size.width);
+// console.log(size.height);
 
-size.resize(1920, 1080);
+// size.resize(1920, 1080);
 
-console.log(size.width);
-console.log(size.height);
+// console.log(size.width);
+// console.log(size.height);
 
-const point = new Position();
-console.log(point.x);
-console.log(point.y);
+// const point = new Position();
+// console.log(point.x);
+// console.log(point.y);
 
-point.move(100, 200);
+// point.move(100, 200);
 
-console.log(point.x);
-console.log(point.y);
+// console.log(point.x);
+// console.log(point.y);
+
+
+// Clase ProgramWindow
+class ProgramWindow {
+    
+    // valores por defecto para width y height
+    constructor(width = 800, height = 600) {
+        this.screenSize = new Size(width, height);
+        this.size = new Size();
+        this.position = new Position();
+    }
+
+    // Método para cambiar el tamaño de la ventana
+    resize(newSize) {
+        // Si el nuevo tamaño es menor a 1, se establece en 1
+        newSize.width = newSize.width < 1 ? 1 : newSize.width;
+        newSize.height = newSize.height < 1 ? 1 : newSize.height;
+
+        // Si el nuevo tamaño es mayor al tamaño de la pantalla, se establece el tamaño de la pantalla
+        if (this.position.x + newSize.width > this.screenSize.width) {
+            newSize.width = this.screenSize.width - this.position.x;
+        }
+        if (this.position.y + newSize.height > this.screenSize.height) {
+            newSize.height = this.screenSize.height - this.position.y;
+        }
+
+        // Se establece el nuevo tamaño
+        this.size.resize(newSize.width, newSize.height);
+    }
+}
+
+const programWindow = new ProgramWindow();
+console.log(programWindow.screenSize.width);
+
+const newSize = new Size(600, 400);
+programWindow.resize(newSize);
+console.log(programWindow.size.width);
+console.log(programWindow.size.height);
