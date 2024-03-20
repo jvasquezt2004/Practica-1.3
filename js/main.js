fda4dd1727ac447c53a2ce29a8b70097cbe5019f
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-03-19 18:32:48 -0600

    Seccion 2 realizada y probada con exito

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ class Size {
     }
 }
 
+class Position {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    move(newX, newY) {
+        this.x  = newX;
+        this.y = newY;
+    }
+}
+
 const size = new Size(1080, 764);
 console.log(size.width);
 console.log(size.height);
@@ -18,3 +30,12 @@ size.resize(1920, 1080);
 
 console.log(size.width);
 console.log(size.height);
+
+const point = new Position();
+console.log(point.x);
+console.log(point.y);
+
+point.move(100, 200);
+
+console.log(point.x);
+console.log(point.y);
